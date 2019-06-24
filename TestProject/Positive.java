2283bb3f735b89b6398a42c9e9e3a58e8d977f18
hi <hi@192.168.0.5>
2019-06-24 23:43:15 +0530

    added class Positive.java

---
+++ TestProject/Positive.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Positive {
+	public static void main(String[] args){
+		int i ;
+		Scanner k = new Scanner(System.in);
+		i = k.nextInt();
+		if(i>0)
+		{
+			System.out.println("i is a positive number");
+			
+		}
+		else if(i<0){
+			System.out.println(" i is a negative number");
+			
+			
+			
+		}
+		else{
+			System.out.println("i is a zero");
+
+}
+	}
+}
